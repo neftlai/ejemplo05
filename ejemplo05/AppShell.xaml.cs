2e64641d837c642d9cf4199e216ec2548fcd338a
50371 <50371@LAPTOP-DE-ORTIX>
2025-07-28 17:27:06 -0600

    Agregar archivos de proyecto.

---
+++ ejemplo05/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ejemplo05
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ejemplo05/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace ejemplo05
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+#pragma warning disable CS8602 // Desreferencia de una referencia posiblemente NULL.
+            (sender as Button).Text = "presioname de nuevo";
+#pragma warning restore CS8602 // Desreferencia de una referencia posiblemente NULL.
+        }
+    }
+
+}
